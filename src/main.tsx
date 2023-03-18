cc76180317fcf62d86a1d285337d64212efb3e74
Cesar Funes <fuunesto@gmail.com>
2023-03-17 19:55:57 -0600

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import ReactDOM from "react-dom/client"
 import Home from "./Home"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, createHashRouter, Navigate, RouterProvider } from "react-router-dom"
 import "./index.css"
 import { WrongSeason } from "./components/WrongSeason"
 import { Club } from "./Club"
 
 const lastYear = new Date().getFullYear() - 1
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/:year",
     element: <Home />,
